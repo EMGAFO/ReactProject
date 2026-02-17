e9854e5450c533db2370673587cc477f51c760be
EMGAFO <emmanuelfortuna68@gmail.com>
2026-02-16 22:43:29 -0400

    extend the test

--- src/FirstStepsApp.tsx
+++ src/FirstStepsApp.tsx
@@ -5,11 +5,20 @@ export function FirstStepsApp() {
     return (
     <>
         <h1>Carrito de compras</h1>
-
-        <ItemCounter name="Nintendo Switch 2" quantity={3}/>
-        <ItemCounter name="Pro Controller" quantity={23} />
-        <ItemCounter name="Super Smash"  quantity={9}/>
-        <ItemCounter name="Super Mario"  quantity={8}/>
+        <ItemCounter name="Zelda: Tears of Kingdom" quantity={5} />
+        <ItemCounter name="Mario Kart 8 Deluxe" quantity={12} />
+        <ItemCounter name="Animal Crossing" quantity={7} />
+        <ItemCounter name="Pokemon Scarlet" quantity={15} />
+        <ItemCounter name="Pokemon Violet" quantity={14} />
+        <ItemCounter name="Splatoon 3" quantity={6} />
+        <ItemCounter name="Metroid Dread" quantity={4} />
+        <ItemCounter name="Joy-Con Pair" quantity={20} />
+        <ItemCounter name="Carry Case" quantity={30} />
+        <ItemCounter name="Screen Protector" quantity={50} />
     </>
  );
 }       
+
+
+
+
