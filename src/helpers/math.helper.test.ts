c96eb5357354e0565d91d495dbbb25d2c45dbe2a
EMGAFO <emmanuelfortuna68@gmail.com>
2026-02-23 00:12:38 -0400

    new features

---
+++ src/helpers/math.helper.test.ts
@@ -0,0 +1,68 @@
+import { describe, expect, test }  from 'vitest';
+import { add, subtract, multiply} from './math.helper';
+
+describe('add', () => {
+    test('Should add two positives numbers', () => {
+        
+    //! 1. Arrange 
+    const a = 1;
+    const b = 2;  
+
+    //! 2. Act  
+    const result = add(a, b);
+
+    //! 3. Assert 
+    expect(result).toBe(a + b)
+    });
+});
+
+
+describe('sustract', () => {
+    test('Should substract two positives numbers', () => {
+
+    //! 1. Arrange 
+    const a = 2;
+    const b = 1
+    //! 2. Act
+    const result = subtract(a, b);
+    //! 3. Assert
+    expect(result).toBe(a - b)
+    });
+
+    test('Should substract two negative numbers', () => {
+
+    //! 1. Arrange 
+    const a = -2;
+    const b = -1
+    //! 2. Act
+    const result = subtract(a, b);
+    //! 3. Assert
+    expect(result).toBe(a - b)
+    });
+}); 
+
+
+describe('multiply', () => {
+    test('Should multiply two numbers', () => {
+    //! 1. Arrange 
+    const a = 2;
+    const b = 1
+    //! 2. Act
+    const result = multiply(a, b);
+    //! 3. Assert
+    expect(result).toBe(a * b)
+    });
+
+    test('Should multiply two negative numbers', () => {
+    //! 1. Arrange 
+    const a = -2;
+    const b = -1
+    //! 2. Act
+    const result = multiply(a, b);
+    //! 3. Assert
+    expect(result).toBe(a * b)
+    });
+}); 
+
+
+
